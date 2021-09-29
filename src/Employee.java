f1f7261ed5f50d3d1ab92803f7d81a74d0f4deea
Bibek-ops <78839583+Bibek-ops@users.noreply.github.com>
2021-09-29 12:17:49 +0545

    second commit and push

--- src/Employee.java
+++ src/Employee.java
@@ -1,39 +1,33 @@
-import java.awt.EventQueue;
+DefaultTableModel model = new DefaultTableModel();
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
-import java.awt.Font;
-import javax.swing.table.DefaultTableModel;
+/**
+ * Launch the application.
+ */
 
-import employeeData.EmployeeData;
+public void updateTable()
+		{
 
-import java.awt.event.ActionListener;
-import java.text.MessageFormat;
-import java.awt.event.ActionEvent;
-import java.sql.*;
-import javax.swing.*;
+		conn = EmployeeData.ConnectDB();
+		if (conn !=null)
+		{
+		String sql =" Select EmpID, NINumber,Firstname,Surname,Gender,DOB,Age,Salary";
 
-public class Employee {
+		try
+		{
+		pst = conn.prepareStatement(sql);
+		rs = pst.executeQuery();
+		Object[]columnData = new Object[8];
 
-	private JFrame frame;
-	private JTextField jtxtEmployeeID;
-	private JTable table;
-	private JTextField jtxtNINumber;
-	private JTextField jtxtFirstname;
-	private JTextField jtxtSurname;
-	private JTextField jtxtGender;
-	private JTextField jtxtDOB;
-	private JTextField jtxtAge;
-	private JTextField jtxtSalary;
-	
-	Connection conn = null;
-	PreparedStatement pst = null;
-	ResultSet rs = null;
-	
-	
-	
+		while(rs.next()) {
+		columnData [0] = rs.getString("EmpID");
+		columnData [1] = rs.getString("NINumber");
+		columnData [2] = rs.getString("Firstname");
+		columnData [3] = rs.getString("Surname");
+		columnData [4] = rs.getString("Gender");
+		columnData [5] = rs.getString("DOB");
+		columnData [6] = rs.getString("Age");
+		columnData [7] = rs.getString("Salary");
+
+		model.addRow(columnData);
+		}
+		}
